dfcce2b68b5d538957d23403e3e8c9ba3adc86f6
go0leum <mijin575@konkuk.ac.kr>
2024-10-25 17:04:14 +0900

    Add

---
+++ utils/evaluation_utils.py
@@ -0,0 +1,13 @@
+from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def evaluation(y_true, y_pred, n, k):
+    
+    mae = mean_absolute_error(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = root_mean_squared_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    adjusted_r2 = (1-r2)(n-1)/(n-k-1)
+
+    return mae, mse, rmse, r2, adjusted_r2
