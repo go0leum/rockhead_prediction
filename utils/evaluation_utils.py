450b8db3d8085947d2ba33d44e47db8f90e71cc2
go0leum <mijin575@outlook.com>
2024-10-27 02:31:14 +0900

    Update:evaluation

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -8,6 +8,14 @@ def evaluation(y_true, y_pred, n, k):
     mse = mean_squared_error(y_true, y_pred)
     rmse = root_mean_squared_error(y_true, y_pred)
     r2 = r2_score(y_true, y_pred)
-    adjusted_r2 = (1-r2)(n-1)/(n-k-1)
+    adjusted_r2 = (1-r2)*(n-1)/(n-k-1)
 
     return mae, mse, rmse, r2, adjusted_r2
+
+def barplot(data, x_col, y_col, figure_name, hue=None):
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=x_col, y=y_col, data=data, hue=hue)
+    plt.title(figure_name)
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.show()
