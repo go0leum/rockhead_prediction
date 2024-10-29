277fef54fed9f9a6bb038bdfa1468c5093556862
go0leum <mijin575@outlook.com>
2024-10-29 10:39:10 +0900

    Update:evaluation

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -2,6 +2,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_s
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
+import utm # 좌표계 변환
 
 def evaluation(y_true, y_pred, n, k):
     
@@ -29,6 +30,14 @@ def boxplot_2(data, x_col, y_col, figure_name):
     plt.ylabel(y_col)
     plt.show()
 
+def distribution_plot(data, x_col, y_col, figure_name):
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x=x_col, y=y_col, data=data)
+    plt.title(figure_name)
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.show()
+
 def surface_3d(data, x_col, y_col, z_col):
     fig = go.Figure(data=[go.Surface(z=data[z_col], x=data[x_col], y=data[y_col])])
     fig.update_layout(
@@ -42,8 +51,9 @@ def surface_3d(data, x_col, y_col, z_col):
     fig.show()
 
 def contour_plot(data, x_col, y_col, z_col):
+
     fig = go.Figure(data=go.Contour(
-        z=data[z_col], x=data[x_col], y=data[y_col],
+        z= utm.to_latlon(data[z_col]), x=utm.to_latlon(data[x_col]), y=utm.to_latlon(data[y_col]),
         colorscale='Viridis',
         contours=dict(
             start= -1,
