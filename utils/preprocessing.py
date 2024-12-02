64b8e9b9d90bd76f6dd2ad1785589771835bac38
go0leum <mijin575@gmail.com>
2024-12-03 00:00:40 +0900

    Update:eda

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -21,9 +21,8 @@ def remove_outlier(data, figure_name):
     return_data = return_data.drop(index=lower_array)
 
     return data, outliers
-    
-def anseline_moran_I(distance_range, data):
 
+def connected_distance(min_distance, max_distance, term, data):
     disconnected_flag = 0
     island_flag = 0
 
@@ -34,28 +33,21 @@ def anseline_moran_I(distance_range, data):
 
     y = gdf['depth_start']  # 분석할 변수명을 지정
 
-    for distance in tqdm(distance_range):
+    for distance in tqdm(range(min_distance, max_distance, term)):
         # 고정 거리 밴드 가중치 행렬 
         w = ps.weights.DistanceBand.from_dataframe(gdf, threshold=distance, silence_warnings = True)
         
         island_count = len(w.islands)
         min_neighbors = w.min_neighbors
-        
+    
         if island_count == 0 and island_flag == 0:
             island_flag = 1
             island_distance = distance
             
-        elif min_neighbors > 1 and disconnected_flag == 0:
+        if min_neighbors > 1 and disconnected_flag == 0:
             disconnected_flag = 1
             disconnected_distance = distance
-            lisa = esda.Moran_Local(y, w, permutations=499)
-
-            neighbors_count = [len(neighbors) for neighbors in w.neighbors.values()]
-            data['neighbors'] = neighbors_count
-            data['moran_I'] = lisa.Is
-            data['p-value'] = lisa.p_sim
-            data['z-score'] = lisa.z_sim
-            data['cotype'] = lisa.q
+            break
 
     if island_flag == 1:
         print(f"not island minimum distance band: {island_distance}m")
@@ -64,7 +56,31 @@ def anseline_moran_I(distance_range, data):
         print(f"all connected minimum distance band: {disconnected_distance}m")
 
     if island_flag == 0 and disconnected_flag == 0:
-        print(f"There is no minimum distance in distance_range {distance_range}")
+        print(f"There is no minimum distance in distance_range [{min_distance}, {max_distance}, {term}]")
         return None
 
-    return disconnected_distance, data
+    return disconnected_distance
+
+
+    
+def anseline_moran_I(distance, data):
+    data_copy = data[['x','y','depth_start']]
+
+    # GeoDataFrame으로 변환
+    gdf = gpd.GeoDataFrame(data_copy, geometry=gpd.points_from_xy(data_copy.x, data_copy.y))
+
+    y = gdf['depth_start']  # 분석할 변수명을 지정
+
+    w = ps.weights.DistanceBand.from_dataframe(gdf, threshold=distance, silence_warnings = True)
+    
+    lisa = esda.Moran_Local(y, w, permutations=9999)
+
+    neighbors_count = [len(neighbors) for neighbors in w.neighbors.values()]
+    data['neighbors'] = neighbors_count
+    data['moran_I'] = lisa.Is
+    data['p-value'] = lisa.p_sim
+    data['z-score'] = lisa.z_sim
+    data['cotype'] = lisa.q
+
+    return data
+
