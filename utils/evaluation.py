37ebecbf0225481ca0b03a64ddb29fba376de0de
go0leum <mijin575@gmail.com>
2024-12-03 00:01:29 +0900

    update:evaluation

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 
-def evaluation(y_true, y_pred, n, k):
+def metrics(y_true, y_pred, n, k):
     
     mae = mean_absolute_error(y_true, y_pred)
     mse = mean_squared_error(y_true, y_pred)
